fca3df7ec5ae212b65c44ad0436ae41b5115c977
Elijah Insua <tmpvar@gmail.com>
2015-12-18 00:44:03 -0700

    different colors / spacing

--- demo.js
+++ demo.js
@@ -12,27 +12,25 @@ QuadTree.QuadTreeNode.prototype.render = function render(ctx) {
     child && child.render(ctx);
   })
 
-  ctx.strokeStyle = 'hsl(9, 100%, 63%)'
-  ctx.fillStyle = 'hsla(210, 100%, 63%, .65)'
+
 
   if (this.leaf && this.occupied) {
+    ctx.strokeStyle = 'hsl(210, 100%, 63%)'
+    ctx.fillStyle = 'hsla(210, 90%, 63%, .65)'
     ctx.fillRect(x-r + 2, y-r + 2, r*2 - 4, r*2 - 4);
   } else if (!this.leaf) {
-    ctx.beginPath()
-      ctx.moveTo(x - r + 2, y);
-      ctx.lineTo(x - 2, y);
-      ctx.moveTo(x + 2, y);
-      ctx.lineTo(x + r - 2, y);
+    ctx.save()
+      ctx.lineWidth = .5
+      ctx.strokeStyle = 'rgba(100, 100, 100, .6)'
+      ctx.beginPath()
+        ctx.moveTo(x - r + 1, y);
+        ctx.lineTo(x + r - 1, y);
 
-      ctx.moveTo(x, y - r + 2);
-      ctx.lineTo(x, y - 2);
-      ctx.moveTo(x, y + 2);
-      ctx.lineTo(x, y + r - 2);
+        ctx.moveTo(x, y - r + 1);
+        ctx.lineTo(x, y + r - 1);
 
-    ctx.stroke()
-
-    ctx.fillStyle = 'hsl(350, 100%, 63%)'
-    ctx.fillRect(x-1, y-1, 2, 2)
+      ctx.stroke()
+    ctx.restore()
   }
 }
 
@@ -46,6 +44,7 @@ tree.add(80, 5)
 tree.add(100, 5)
 tree.add(120, 5)
 tree.add(240, 5)
+tree.add(240, 50)
 
 
 
@@ -60,7 +59,7 @@ var ctx = fc(function() {
   var r = tree.root.radius;
   var x = tree.root.center[0];
   var y = tree.root.center[1];
-  ctx.strokeStyle = 'hsla(90, 100%, 63%, 1)'
+  ctx.strokeStyle = 'hsla(90, 100%, 63%, .75)'
 
   ctx.strokeRect(x-r, y-r, r*2, r*2);
 })
